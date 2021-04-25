127b729c4a14d62d2065d723e95ce01b7b1a2920
DESKTOP-9B4631F\vkurk <v.kurko2012@gmail.com>
2021-04-25 20:00:54 +0300

    create PageNot found component

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const PageNotFound = () => (
+    <div>
+        <h1>404</h1>
+        <h3>Page not found</h3>
+        <p>Try to return the 
+         <Link to="/" >home page</Link>
+        </p>
+    </div>
+)
+
+export default PageNotFound;
