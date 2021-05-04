e794ca5d5d28918d183e18a99469723d3389f203
DESKTOP-9B4631F\vkurk <v.kurko2012@gmail.com>
2021-05-04 11:29:14 +0300

    fix 404 page

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -6,7 +6,7 @@ const PageNotFound = () => (
         <h1>404</h1>
         <h3>Page not found</h3>
         <p>Try to return the 
-         <Link to="/" >home page</Link>
+         <Link to="/" > home page</Link>
         </p>
     </div>
 )
