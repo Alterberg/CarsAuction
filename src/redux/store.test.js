d5f6e9dff6769a2a23bcd31747c99c1a7454f2e1
DESKTOP-9B4631F\vkurk <v.kurko2012@gmail.com>
2021-05-16 11:28:25 +0300

    inegrated redux test

---
+++ src/redux/store.test.js
@@ -0,0 +1,20 @@
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+import initialState from "./reducers/initialState";
+import * as carActions from "./actions/carActions";
+
+it("Should handle creating cars", function () {
+    // arrange
+    const store = createStore(rootReducer, initialState);
+    const car = {
+        model: "Tesla"
+    };
+
+    // act
+    const action = carActions.createCarSuccess(car);
+    store.dispatch(action);
+
+    // assert
+    const createdCar = store.getState().cars[0];
+    expect(createdCar).toEqual(car);
+})
